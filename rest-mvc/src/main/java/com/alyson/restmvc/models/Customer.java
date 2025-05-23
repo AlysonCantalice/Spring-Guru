50401286613f185e58dab76b9eabb1662bee1e37
Alyson <alysoncantalice@gmail.com>
2025-05-23 08:15:39 -0300

    Fixed variable typo

--- rest-mvc/src/main/java/com/alyson/restmvc/models/Customer.java
+++ rest-mvc/src/main/java/com/alyson/restmvc/models/Customer.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 @Data
 @Builder
 public class Customer {
-    private String costumerName;
+    private String customerName;
     private UUID id;
     private Integer version;
     private LocalDateTime createdDate;
